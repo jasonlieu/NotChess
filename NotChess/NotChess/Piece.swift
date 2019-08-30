04a1f9b23a632519b96ab84bf65e8d091763e642
jasonlieu <jasontulieu@gmail.com>
2019-08-29 17:11:11 -0700

    started enemy movement

--- NotChess/NotChess/Piece.swift
+++ NotChess/NotChess/Piece.swift
@@ -10,14 +10,62 @@ import UIKit
 
 class Piece{
     var pieceType : String?
+    var direction : String?
     var pX : Int?
     var pY : Int?
-    init(type: String, positionX : Int, positionY: Int){
+    var delay : Int = 0
+    init(type: String, direction: String, positionX : Int, positionY: Int){
         pieceType = type
+        self.direction = direction
         pX = positionX
         pY = positionY
     }
-    func move(){
+    func checkMove(){
+        switch pieceType{
+        case "pawn":
+            if delay <= 4 {
+                movePawn()
+            }
+        case "rook":
+            if delay <= 3 {
+                moveRook()
+            }
+        case "knight":
+            if delay <= 2 {
+                moveKnight()
+            }
+        case "bishop":
+            if delay <= 2 {
+                moveBishop()
+            }
+        case "king":
+            if delay <= 4 {
+                moveKing()
+            }
+        case "queen":
+            if delay <= 1 {
+                moveQueen()
+            }
+        default:
+            print("invalid piece")
+        }
+    }
+    func movePawn(){
+        
+    }
+    func moveRook(){
+        
+    }
+    func moveKnight(){
+        
+    }
+    func moveBishop(){
+        
+    }
+    func moveKing(){
+        
+    }
+    func moveQueen(){
         
     }
 }
